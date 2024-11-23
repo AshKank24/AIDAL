3ca34ff815ff52ef040eb1b0496814b4b488ef49
Dharin Dave <dharin.dave01@gmail.com>
2024-11-23 19:57:59 +0530

    changed search engine

--- src/summarization.py
+++ src/summarization.py
@@ -4,6 +4,7 @@ from duckduckgo_search import DDGS
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+from googleapiclient.discovery import build
 
 load_dotenv()
 
@@ -12,9 +13,13 @@ genai.configure(api_key=os.environ["GEMINI"])
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 def search_dsa_topic(topic):
-    with DDGS() as ddgs:
-        results = list(ddgs.text(f"{topic} data structures and algorithms", max_results=4))
-    return [result['href'] for result in results]
+    my_api_key = os.environ["GOOGLE_API_KEY"]
+    my_cse_id = os.environ["GOOGLE_CSE_ID"]
+    
+    service = build("customsearch", "v1", developerKey=my_api_key)
+    results = service.cse().list(q=f"{topic} data structures and algorithms", cx=my_cse_id, num=4).execute()
+    
+    return [result['link'] for result in results['items']]
 
 def scrape_content(url):
     response = requests.get(url)
